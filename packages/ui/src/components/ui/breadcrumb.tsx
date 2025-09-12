112aa25738ea26d073421cac2950e8702c50058b
Reyser Zapata <reyserlyn@gmail.com>
2025-09-12 17:18:15 -0500

    feat(ui): agregar nuevos componentes y dependencias

--- packages/ui/src/components/ui/breadcrumb.tsx
+++ packages/ui/src/components/ui/breadcrumb.tsx
@@ -1,5 +1,5 @@
 import { Slot } from "@radix-ui/react-slot";
-import { cn } from "@renovabit/ui/utils/cn";
+import { cn } from "@renovabit/ui/utils/cn/cn";
 import { ChevronRight, MoreHorizontal } from "lucide-react";
 import * as React from "react";
 
